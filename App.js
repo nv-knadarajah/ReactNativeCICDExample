4efe17da38ae249c7f88ab7f696f7bdc1864d552
nv-knadarajah <kughan.nadarajah@nventive.com>
2021-09-05 15:49:23 -0400

    Feature 1 ready!

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Greeting from './components/greeting';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput value="Kughan Nadarajah"/>
-      <Text>Welcome to this CI/CD example!</Text>
-      <StatusBar style="auto" />
-    </View>
+ <Greeting/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/greeting.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class Greeting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: ''}
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <TextInput style={{ height: 30, borderColor: 'gray', borderWidth: 1 }}
+                    placeholder="Write your name..."
+                    onChangeText={(name) => this.setState({ name })} />
+                <Text style={{marginTop: 10}}>Welcome to this CI/CD example, {this.state.name}!</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        marginHorizontal: 16,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'stretch',
+        justifyContent: 'flex-start',
+    },
+});
